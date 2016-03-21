5d6466f29df8b8789d2b64d804a223e95a5165c1
Stephen Lujan <StephenLujan@gmail.com>
2016-03-21 11:22:32 -0400

    using karma-typescript-preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,8 @@ module.exports = function(config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/**/*.js': ['babel'],
-      'src/**/*.ts': ['babel']
+      'src/**/*.ts': ['karma-typescript-preprocessor']
+      //typescript preprocessors [karma-typescript-preprocessor, karma-simpletsc-preprocessor, karma-tsc-preprocessor, karma-typescript-preprocessor-without-copy]
     },
     'babelPreprocessor': {
       options: {
@@ -47,6 +48,26 @@ module.exports = function(config) {
         ]
       }
     },
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
+      options: {
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'ES5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'amd', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: true, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      typings: [
+        'typings/tsd.d.ts'
+      ],
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.ts$/, '.js');
+      }
+    },
+
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
